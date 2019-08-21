938fae5e9688b358660c1ba159c2a54493bc4b65
Andrew Welch <andrew@nystudio107.com>
2019-08-21 09:57:51 -0400

    Add VuePress docs
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Empty Coalesce Documentation',
+    description: 'Documentation for the Empty Coalesce plugin',
+    base: '/docs/empty-coalesce/',
+    dest: '../../../sites/nystudio107/web/docs/empty-coalesce',
+    themeConfig: {
+        sidebar: [
+            ['/', 'Empty Coalesce plugin for Craft CMS 3.x'],
+        ]
+    }
+};
