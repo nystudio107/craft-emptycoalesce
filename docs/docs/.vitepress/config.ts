89f55426e7e21691fb8b33105a2276890be1ce73
Andrew Welch <andrew@nystudio107.com>
2024-04-11 21:37:24 -0400

    docs: Updated docs to use node 20 & a new sitemap plugin

---
+++ docs/docs/.vitepress/config.ts
@@ -0,0 +1,48 @@
+import {defineConfig} from 'vitepress'
+
+export default defineConfig({
+  title: 'Empty Coalesce Plugin',
+  description: 'Documentation for the Empty Coalesce plugin',
+  base: '/docs/empty-coalesce/v4/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://twitter.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://youtube.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/nystudio107'},
+      {icon: 'twitter', link: 'https://twitter.com/nystudio107'},
+    ],
+    logo: '/img/plugin-logo.svg',
+    editLink: {
+      pattern: 'https://github.com/nystudio107/craft-emptycoalesce/edit/develop-v4/docs/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    algolia: {
+      appId: 'L15LEW8LWP',
+      apiKey: '39198fa2e54d3da8c644d9cd241cff5a',
+      indexName: 'nystudio107-empty-coalesce',
+      searchParameters: {
+        facetFilters: ["version:v4"],
+      },
+    },
+    lastUpdatedText: 'Last Updated',
+    sidebar: [],
+    nav: [
+      {text: 'Home', link: 'https://nystudio107.com/plugins/empty-coalesce'},
+      {text: 'Store', link: 'https://plugins.craftcms.com/empty-coalesce'},
+      {text: 'Changelog', link: 'https://nystudio107.com/plugins/empty-coalesce/changelog'},
+      {text: 'Issues', link: 'https://github.com/nystudio107/craft-empty-coalesce/issues'},
+      {
+        text: 'v4', items: [
+          {text: 'v5', link: 'https://nystudio107.com/docs/empty-coalesce/'},
+          {text: 'v4', link: '/'},
+          {text: 'v1', link: 'https://nystudio107.com/docs/empty-coalesce/v1/'},
+        ],
+      },
+    ]
+  },
+});
